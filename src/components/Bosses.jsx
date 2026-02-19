5de1dbac7eb8313f6da6774e5c225cd17d0ef01a
Jen G <southpawgeek@users.noreply.github.com>
2026-02-18 21:37:30 -0500

    add boss deaths

--- src/components/Bosses.jsx
+++ src/components/Bosses.jsx
@@ -1,9 +1,10 @@
 import useSaveProvider from "../hooks/useSaveProvider"
 
-const BossCard = ({ label, description, acquired }) => {
+const BossCard = ({ label, description, acquired, tried }) => {
   return (
     <div
       style={{
+        position: "relative",
         border: acquired ? "1px solid #ffc" : "1px solid #666",
         padding: "0.5rem",
         borderRadius: "0.5rem",
@@ -19,6 +20,22 @@ const BossCard = ({ label, description, acquired }) => {
           </p>
         </>
       )}
+      {tried > 0 && (
+        <>
+          <br />
+          <span
+            style={{
+              position: "absolute",
+              bottom: "0.25rem",
+              right: "0.25rem",
+              color: "#ffc",
+              fontSize: "0.8rem",
+            }}
+          >
+            {tried} <i className="fa-solid fa-skull" />
+          </span>
+        </>
+      )}
     </div>
   )
 }
@@ -28,6 +45,7 @@ const Bosses = () => {
   const bossesList = collectibles?.bosses ?? []
   const bossDetails = collectibles?.boss ?? {}
   const bossesDefeated = playerStats?.bossesDefeated ?? {}
+  const bossesTried = playerStats?.bossesTried ?? {}
 
   const defeatedCount = bossesList.filter(
     (id) => (bossesDefeated[id] ?? 0) > 0,
@@ -54,12 +72,14 @@ const Bosses = () => {
           const name = info.name ?? bossId
           const description = info.description ?? ""
           const acquired = (bossesDefeated[bossId] ?? 0) > 0
+          const tried = bossesTried[bossId] ?? 0
           return (
             <BossCard
               key={bossId}
               label={name}
               description={description}
               acquired={acquired}
+              tried={tried}
             />
           )
         })}
