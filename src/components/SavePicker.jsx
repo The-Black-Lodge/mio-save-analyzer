e3b9b2d5833bd17c2bda2012f17f670493f7e8e0
Jen Guerra <southpawgeek@users.noreply.github.com>
2026-03-07 11:43:00 -0500

    add steam deck

--- src/components/SavePicker.jsx
+++ src/components/SavePicker.jsx
@@ -29,34 +29,46 @@ const SavePicker = () => {
     <>
       <input
         ref={fileInputRef}
-        type="file"
-        accept=".save"
-        className="hidden"
+        type='file'
+        accept='.save'
+        className='hidden'
         onChange={handleFileChange}
       />
-      <div className="upload-section">
+      <div className='upload-section'>
         <button
           onClick={handleUploadClick}
-          className="upload-btn"
+          className='upload-btn'
         >
           Upload Save
         </button>
-        {error && <p className="text-small text-warning">{error}</p>}
+        {error && <p className='text-small text-warning'>{error}</p>}
         <span
-          className="save-path-tooltip"
+          className='save-path-tooltip'
           tabIndex={0}
         >
           Where is my save?
-          <p className="save-path-tooltip-content">
-            Save files are usually stored in
-            <br />
-            Windows/Steam:
-            <br />
-            C:\Users\(Username)\AppData\Local\MIO\Saves\Steam\(Steam64ID)\slot_(0-2).save
-            <br />
-            Windows/Xbox Game Pass:
-            <br />
-            C:\Users\(Username)\AppData\Local\Packages\FocusHomeInteractiveSA.MioMemoriesInOrbit_(hash)/SystemAppData\wgs(User ID)(Save Slot Id)
+          <p className='save-path-tooltip-content'>
+            Save files are usually stored in:
+            <p>
+              <i class='fa-brands fa-windows'></i>
+              <i class='fa-brands fa-steam'></i> Windows/Steam:
+              <br />
+              C:\Users\(Username)\AppData\Local\MIO\Saves\Steam\(Steam64ID)\slot_(0-2).save
+            </p>
+            <hr className='divider' />
+            <p>
+              <i class='fa-brands fa-windows'></i>
+              <i class='fa-brands fa-xbox'></i> Windows/Xbox Game Pass:
+              <br />
+              C:\Users\(Username)\AppData\Local\Packages\FocusHomeInteractiveSA.MioMemoriesInOrbit_(hash)/SystemAppData\wgs\(User
+              ID)\(Save Slot Container Id)
+            </p>
+            <hr className='divider' />
+            <p>
+              <i class='fa-brands fa-steam'></i>Steam Deck:
+              <br />
+              ./.local/share/Steam/steamapps/compatdata/1672810/pfx/drive_c/users/steamuser/AppData/Local/MIO/Saves/Steam/(Steam64ID)/slot_(0-2).save
+            </p>
           </p>
         </span>
       </div>
