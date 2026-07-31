9489389436cb273fedd93f8f1057d427293ba755
Jen G. <southpawgeek@users.noreply.github.com>
2026-07-30 22:29:20 -0400

    update site footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <footer className="footer">
+      <span className="footer-text">
+        © {new Date().getFullYear()} &nbsp; <a href="https://southpawgeek.com/" target="_blank" rel="noopener noreferrer">J. A. Guerra</a>
+      </span>
+      <span className="footer-separator">·</span>
       <a
+        className="footer-icon-link"
         href="https://github.com/The-Black-Lodge/mio-save-analyzer"
         target="_blank"
         rel="noopener noreferrer"
@@ -9,6 +14,8 @@ const Footer = () => {
       >
         <i className="fa-brands fa-github" />
       </a>
+            <span className="footer-separator">·</span>
+<span className="footer-text"><a href="https://diatomravine.com/games" target="_blank" rel="noopener noreferrer">Diatom Ravine</a></span>
     </footer>
   )
 }
